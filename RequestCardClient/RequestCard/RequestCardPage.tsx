3238401ff70d29429ff2b2e78a758d59bceb4831
Hleb-s <Ivan_Khlebushkin@EPKZKARW0468>
2016-09-08 12:34:04 +0600

    update typescript version 1.8.6

--- RequestCardClient/RequestCard/RequestCardPage.tsx
+++ RequestCardClient/RequestCard/RequestCardPage.tsx
@@ -9,9 +9,9 @@ import {Approve} from "./approve";
 import {NewClient} from "./newClient";
 import {Revision, IFullCardInfo} from "./revision";
 import {ICommentForStepModel} from "./commentForStep";
-import {Test} from "../../Client/Core/utils";
+//import {Test} from "../../Client/Core/utils";
 
-alert(Test());
+//alert(Test());
 
 interface IUserTask {
     id: string;
