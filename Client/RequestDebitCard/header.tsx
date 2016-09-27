044de1239a3be3d4378e0e299422a01cb758f664
Hleb-s <Ivan_Khlebushkin@EPKZKARW0468>
2016-09-27 11:27:27 +0600

    add requestclientinfo view

---
+++ Client/RequestDebitCard/header.tsx
@@ -0,0 +1,13 @@
+﻿import * as React from "react";
+import { Router, Route, Link } from 'react-router'
+
+export class Header extends React.Component<{}, {}> {
+    constructor() {
+        super();
+    }
+
+    render() {
+        return <div className="">HalykBANK</div>;
+
+    }
+}
